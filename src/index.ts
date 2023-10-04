3f4f536ed5d81cba037e8fb22854db1aaf203463
Jiakun Zhao <50269993+jiakun-zhao@users.noreply.github.com>
2023-10-04 17:23:23 +0800

    chore: update deps

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,10 @@ import type { Processor } from 'unified'
 import { jsx, toJs } from 'estree-util-to-js'
 import type { Options } from 'hast-util-to-estree'
 import { toEstree } from 'hast-util-to-estree'
-import type { HastNodes } from 'hast-util-to-estree/lib'
 
 export default function rehypeJsx(this: Processor, options?: Options) {
-  this.Compiler = function (hast: HastNodes): string {
-    const tree = toEstree(hast, options ?? { elementAttributeNameCase: 'html', stylePropertyNameCase: 'css' })
+  this.compiler = function (root: any): string {
+    const tree = toEstree(root, options ?? { elementAttributeNameCase: 'html', stylePropertyNameCase: 'css' })
     return toJs(tree, { handlers: jsx }).value
   }
 }
